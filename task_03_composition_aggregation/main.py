e77e4ab148a04ff9f4f1316dc21ae3c38815716d
Maxim Makarov <mmaxim01@yandex.ru>
2026-06-10 20:19:26 +0300

    Созданы основные классы

---
+++ task_03_composition_aggregation/main.py
@@ -0,0 +1,41 @@
+class User:
+    def __init__(self, name: str, email: str):
+        self.name = name
+        self.email = email
+
+
+class Lesson:
+    def __init__(self, title: str, duration: int):
+        self.title = title
+        self.duration = duration
+
+
+class Module:
+    def __init__(self, title: str):
+        self.title = title
+        self.lessons = []
+
+
+class OnlineCourse:
+    def __init__(self, title: str, teacher: User):
+        self.title = title
+        self.teacher = teacher
+        self.students = []
+        self.modules = []
+
+
+
+
+
+
+
+# Агрегация:
+teacher = User("Мария", "maria@example.com")
+student = User("Олег", "oleg@example.com")
+course = OnlineCourse("Основы Python", teacher)
+course.enroll_student(student)
+
+# Композиция:
+module = course.add_module("ООП")
+module.add_lesson("Классы и объекты", 45)
+module.add_lesson("Композиция и агрегация", 60)
