c4752f45e845b74ab3e5c1f8f5e268db405e4aba
Maxim Makarov <mmaxim01@yandex.ru>
2026-06-10 20:25:22 +0300

    Добавлен функционал для добавления уроков и вывода информации о курсе

--- task_03_composition_aggregation/main.py
+++ task_03_composition_aggregation/main.py
@@ -15,6 +15,10 @@ class Module:
         self.title = title
         self.lessons = []
 
+    def add_lesson(self, title: str, duration: int):
+        lesson = Lesson(title, duration)
+        self.lessons.append(lesson)
+
 
 class OnlineCourse:
     def __init__(self, title: str, teacher: User):
@@ -23,10 +27,28 @@ class OnlineCourse:
         self.students = []
         self.modules = []
 
+    def enroll_student(self, student: User):
+        self.students.append(student)
 
 
+    def add_module(self, title: str) -> Module:
+        module = Module(title)
+        self.modules.append(module)
+        return module
 
+    def print_course_info(self):
+        print(f"Курс: {self.title}")
+        print(f"Преподаватель: {self.teacher.name} ({self.teacher.email})")
 
+        print("Студенты:")
+        for student in self.students:
+            print(f"- {student.name} ({student.email})")
+
+        print("Модули:")
+        for module in self.modules:
+            print(f"- {module.title}")
+            for lesson in module.lessons:
+                print(f"  - {lesson.title} ({lesson.duration} мин)")
 
 
 # Агрегация:
@@ -39,3 +61,5 @@ course.enroll_student(student)
 module = course.add_module("ООП")
 module.add_lesson("Классы и объекты", 45)
 module.add_lesson("Композиция и агрегация", 60)
+
+course.print_course_info()
